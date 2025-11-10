37e4a1bd2950b0f3286fe723b84aeae6f02d825e
Anandhu Satheesh <anandhusathe@gmail.com>
2025-11-10 11:41:12 +0530

    ...

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -112,10 +112,10 @@ resource "google_cloud_run_v2_service" "api" {
           path = var.health_check_path
           port = var.api_port
         }
-        initial_delay_seconds = 10
-        timeout_seconds       = 5
-        period_seconds        = 10
-        failure_threshold     = 3
+        initial_delay_seconds = 30
+        timeout_seconds       = 10
+        period_seconds        = 15
+        failure_threshold     = 5
       }
 
       liveness_probe {
